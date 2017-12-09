8aa8cfd7de85313bf744d4442faf0ec30e71665d
Zac Connelly <zac@mimi.io>
2017-12-09 23:47:18 +0100

    very small day today but still here

---
+++ 011_foo/tricks.bash
@@ -0,0 +1,16 @@
+# taken from here
+# https://stackoverflow.com/questions/68372/what-is-your-single-most-favorite-command-line-trick-using-bash
+
+# see all of your shortcuts 
+
+bind -p
+
+# enter the arguments from your last command
+# <esc> .
+
+# ^find^replace in last command e.g.
+
+ehco foo bar
+^ehco^echo
+
+# I also use <ctrl>-r all the time (and pbcopy / pbpaste)
